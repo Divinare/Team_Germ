634374c6a4cbbb85923cc3a17420f6924fd60b27
broileri <jenna.lindh@luukku.com>
2014-09-21 21:46:06 +0300

    added 'unit' tag to units + started working on turns

---
+++ Germs/Assets/Scripts/Turn/TurnHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnHandler : MonoBehaviour {
+
+
+	// oltava lista kaikista otukoista ja niiden nopeuksista
+	// käy otukset läpi niiden nopeuden perusteella
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
+		Debug.Log("LOG: "+ units);
+		Debug.Log("LOG[index]: "+ units[0]);	
+		Debug.Log("MOOOI");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+
+	public void insertionSort (ref int[] A){
+		for (int i = 0; i < A.Length; i++)
+		{
+			int value = A[i], j = i-1;
+			while (j >= 0 && A[j] > value)
+			{
+				A[j + 1] = A[j];
+				j--;
+			}
+			A[j + 1] = value;
+		}
+	}
+
+
+}
