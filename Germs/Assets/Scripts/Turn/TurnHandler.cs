183ff80ef0479734e5ed45fc67d8ea3bdf8dc4b2
broileri <jenna.lindh@luukku.com>
2014-09-22 01:44:23 +0300

    started doing turn stuff

--- Germs/Assets/Scripts/Turn/TurnHandler.cs
+++ Germs/Assets/Scripts/Turn/TurnHandler.cs
@@ -1,43 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System;
+using System.Collections.Generic;
+
 public class TurnHandler : MonoBehaviour {
-
-
+	
+	
 	// oltava lista kaikista otukoista ja niiden nopeuksista
 	// käy otukset läpi niiden nopeuden perusteella
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
+		
 		GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
-		//Debug.Log("Units-lista");
-		//Debug.Log(units);
-		//Debug.Log("Units-listan eka alkio");
-		//Debug.Log("LOG[index]: " + units[0]);	
-		//Debug.Log("Units-listan ekan alkion nopeus");
-		//Debug.Log("LOG[index]: " + units[0].speed);
+		List<GameObject> unitList = new List<GameObject>(units);
+		unitList.Sort(SpeedCompare);
+
+		Debug.Log ("TurnHandler:");
+		Debug.Log("Units-listan PITUUS " + units.Length);
+		Debug.Log ("Units-listan eka alkio " + units [0]);
+		Debug.Log ("EKAN OTUKAN NOPEUS " + unitList[0].transform.GetComponent<UnitStatus>().speed);	
+		Debug.Log ("TOKAN OTUKAN NOPEUS " + unitList[1].transform.GetComponent<UnitStatus>().speed);	
+		Debug.Log ("KOLMANNEN OTUKAN NOPEUS " + unitList[2].transform.GetComponent<UnitStatus>().speed);		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
+		
+	}
 	
+	// Comparison thingy for Sort() - compares the units' speed
+	public int SpeedCompare(GameObject x, GameObject y) {
+		int result = -x.transform.GetComponent<UnitStatus>().speed.CompareTo(y.transform.GetComponent<UnitStatus>().speed);		
+		return result;
 	}
-
-
-	public void insertionSort (ref int[] A){
-		for (int i = 0; i < A.Length; i++)
-		{
-			int value = A[i], j = i-1;
-			while (j >= 0 && A[j] > value)
-			{
-				A[j + 1] = A[j];
-				j--;
-			}
-			A[j + 1] = value;
-		}
-	}
-
-
+	
+	
 }
