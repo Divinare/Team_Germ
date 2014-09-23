2c513d1c76565f13739d806616d75bd7e6769781
broileri <jenna.lindh@luukku.com>
2014-09-24 02:19:23 +0300

    turn handling yay

--- Germs/Assets/Scripts/Turn/TurnHandler.cs
+++ Germs/Assets/Scripts/Turn/TurnHandler.cs
@@ -1,41 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-using System;
 using System.Collections.Generic;
 
 public class TurnHandler : MonoBehaviour {
-	
-	
-	// oltava lista kaikista otukoista ja niiden nopeuksista
-	// käy otukset läpi niiden nopeuden perusteella
-	
-	
-	// Use this for initialization
-	void Start () {
-		
-		GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
-		List<GameObject> unitList = new List<GameObject>(units);
-		unitList.Sort(SpeedCompare);
 
-		Debug.Log ("TurnHandler:");
-		Debug.Log("Units-listan PITUUS " + units.Length);
-		Debug.Log ("Units-listan eka alkio " + units [0]);
-		Debug.Log ("EKAN OTUKAN NOPEUS " + unitList[0].transform.GetComponent<UnitStatus>().speed);	
-		Debug.Log ("TOKAN OTUKAN NOPEUS " + unitList[1].transform.GetComponent<UnitStatus>().speed);	
-		Debug.Log ("KOLMANNEN OTUKAN NOPEUS " + unitList[2].transform.GetComponent<UnitStatus>().speed);		
+	private GameObject activeUnit; // The unit whose turn is active
+	private int unitListIndex;
+	private List<GameObject> unitList;
+	private bool battleIsOver;
+
+		
+	// Puts all units in to a list, sorts them by speed and gives the turn to the fastest one
+	void Start () {
+		battleIsOver = false;
+		GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
+		unitList = new List<GameObject>(units);
+		unitList.Sort(SpeedCompare);
+		initNewRound ();		
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
-		
+
+		// Checks whether the selected unit's turn has ended
+		if (!activeUnit.transform.GetComponent<UnitStatus>().selected) {
+			checkIfBattleOver();
+			// Checks whether round has ended
+			if (unitListIndex == unitList.Count - 1) {
+				trimUnitList();
+			}
+			// Round hasn't ended
+			else {
+				unitListIndex++;
+				// Checks wheter the next unit is still alive so it can be given the next turn
+				if (unitList[unitListIndex] != null) {
+				    activeUnit = unitList[unitListIndex];
+					activeUnit.transform.GetComponent<UnitStatus>().Select();
+				}
+			}
+		}		
 	}
 	
 	// Comparison thingy for Sort() - compares the units' speed
-	public int SpeedCompare(GameObject x, GameObject y) {
+	private int SpeedCompare(GameObject x, GameObject y) {
 		int result = -x.transform.GetComponent<UnitStatus>().speed.CompareTo(y.transform.GetComponent<UnitStatus>().speed);		
 		return result;
+	}
+
+	// Removes the dead creatures (nulls) from the unitList
+	private void trimUnitList() {
+		for (int i = 0; i < unitList.Count; i++) {
+			if (unitList[i] == null) {
+				unitList.RemoveAt(i);
+			}
+		}
+		initNewRound();
+	}
+
+	// Initializes variables for a new game round
+	private void initNewRound() {
+		unitListIndex = 0;
+		activeUnit = unitList[unitListIndex];
+		activeUnit.transform.GetComponent<UnitStatus>().Select();
+	}
+
+	// Assigns true to bool battleIsOver, if either side is defeated
+	private void checkIfBattleOver() {
+		bool playerIsDead = true, enemyIsDead = true;
+		for (int i = 0; i < unitList.Count; i++) {
+			// If the enemy side has unit(s) alive
+			if (unitList[i] != null && unitList[i].transform.GetComponent<UnitStatus>().enemy) {
+				enemyIsDead = false;
+			}
+			// If the player side has unit(s) alive
+			else {
+				playerIsDead = false;
+			}
+		}
+		if (enemyIsDead || playerIsDead) {
+			Debug.Log("BATTLE IS OVER, because no enemy or !enemy units remain");
+			this.battleIsOver = true;
+		}
+	}
+
+	// The unit whose turn it is
+	public GameObject getActiveUnit() {
+		return this.activeUnit;
+	}
+
+	// True if either side has been defeated
+	public bool isBattleOver() {
+		return battleIsOver;
 	}
 	
 	
