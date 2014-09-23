ef219f9a17078bd1f7a7ac14afffd171eacb7fce
smooshi <timere@gmail.com>
2014-09-23 21:41:46 +0300

    main menu

---
+++ Germs/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect(Screen.width /2 - Screen.width/5,Screen.height /2 - Screen.height/3,Screen.width/3,Screen.height - Screen.height/3), "Main Menu");
+		if (GUI.Button (new Rect (Screen.width /2 - Screen.width/5, Screen.height / 2 - Screen.height/4, Screen.width/3, Screen.height/8), "New Game")) {
+		}
+		if (GUI.Button (new Rect (Screen.width /2 - Screen.width/5, Screen.height / 2 - Screen.height/8, Screen.width/3, Screen.height/8), "Load Game")) {
+		}
+		if (GUI.Button (new Rect (Screen.width /2 - Screen.width/5, Screen.height / 2, Screen.width/3, Screen.height/8), "Save Game")) {
+		}
+		if (GUI.Button (new Rect (Screen.width /2 - Screen.width/5, Screen.height / 2 + Screen.height/8, Screen.width/3, Screen.height/8), "Quit Game")) {
+		}
+	}
+}
---
+++ Germs/Assets/Scripts/Main Menu/Menu_movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_movement : MonoBehaviour {
+	private Vector3 mousePos;
+	private float movementSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		if (mousePos != transform.position) {
+			mousePos.z = transform.position.z;
+			transform.position = Vector3.MoveTowards(transform.position, mousePos, movementSpeed * Time.deltaTime);
+		} else {
+			//DIBBADUUU
+		}
+
+	}
+}
