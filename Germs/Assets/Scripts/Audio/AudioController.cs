f28225dac4fd5d3205e686fa1a27e9e0c13017c6
banjohirvi <ollilari@gmail.com>
2014-09-26 00:23:19 +0300

    Sound fixes

---
+++ Germs/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public AudioClip[] audioClips;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
