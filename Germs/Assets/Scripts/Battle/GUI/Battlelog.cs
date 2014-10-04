783034965fcdd16cf0d7c024304f856e2c769e6a
Divinare <niemijoe@hotmail.com>
2014-10-04 13:14:12 +0300

    new battlebackground and battlelog fixed

--- Germs/Assets/Scripts/Battle/GUI/Battlelog.cs
+++ Germs/Assets/Scripts/Battle/GUI/Battlelog.cs
@@ -5,15 +5,6 @@ public class Battlelog : MonoBehaviour {
 	
 	private string battlelog = "";
 	public Vector2 scrollPosition = new Vector2(0,0);
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	public void addToBattleLog(string txt) {
 		if (battlelog.Length == 0) {
@@ -27,24 +18,20 @@ public class Battlelog : MonoBehaviour {
 	void OnGUI() {
 
 
-		float width = (float)(Screen.width * 0.3) + 50;
-		float height = (float)(Screen.height * 0.1);
-		float posY = Screen.height - ((float)(Screen.height * 0.1) + 25);
+		float width = (float)(Screen.width * 0.2);
+		float height = 80;
+		float posY = Screen.height - ((float)(Screen.height * 0.2));
 		float posX = 0;
 
 		GUILayout.BeginArea (new Rect (posX, posY, width, height));
 		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(width), GUILayout.Height(height));
 		GUILayout.Label(battlelog);
-	/*	if (GUILayout.Button ("Clear")) {
-			battlelog = "Battlelog";
-		}
 
-	*/
 		GUILayout.EndScrollView();
 		GUILayout.EndArea();
 
-
-		//GUILayout.BeginArea (new Rect (100, 200, 300, 400));
+		// Text area background
+		GUI.Box(new Rect(posX, posY-1.5f, width, height+2), new GUIContent());
 
 
 	
