b4cbc02717fbbd9d9ce38091523baf187efcf3c5
Divinare <niemijoe@hotmail.com>
2014-10-04 12:18:02 +0300

    battlelog scrollable

--- Germs/Assets/Scripts/Battle/UI/Battlelog.cs
+++ Germs/Assets/Scripts/Battle/UI/Battlelog.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Battlelog : MonoBehaviour {
 	
-	private string battlelog = "Battlelog";
+	private string battlelog = "";
+	public Vector2 scrollPosition = new Vector2(0,0);
 	// Use this for initialization
 	void Start () {
 		
@@ -15,16 +16,37 @@ public class Battlelog : MonoBehaviour {
 	}
 
 	public void addToBattleLog(string txt) {
-		battlelog += "\n" + txt;
+		if (battlelog.Length == 0) {
+						battlelog = txt;
+		} else {
+				battlelog += "\n" + txt;
+		}
+		scrollPosition.y = Mathf.Infinity;
 	}
 	
 	void OnGUI() {
 
 
-		GUI.Box(new Rect(
-			0,
-			Screen.height - ((float)(Screen.height * 0.1) + 25),
-			(float)(Screen.width * 0.3) + 50, (float)(Screen.height * 0.1) + 25),
-		    battlelog);
+		float width = (float)(Screen.width * 0.3) + 50;
+		float height = (float)(Screen.height * 0.1);
+		float posY = Screen.height - ((float)(Screen.height * 0.1) + 25);
+		float posX = 0;
+
+		GUILayout.BeginArea (new Rect (posX, posY, width, height));
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(width), GUILayout.Height(height));
+		GUILayout.Label(battlelog);
+	/*	if (GUILayout.Button ("Clear")) {
+			battlelog = "Battlelog";
+		}
+
+	*/
+		GUILayout.EndScrollView();
+		GUILayout.EndArea();
+
+
+		//GUILayout.BeginArea (new Rect (100, 200, 300, 400));
+
+
+	
 	}
 }
