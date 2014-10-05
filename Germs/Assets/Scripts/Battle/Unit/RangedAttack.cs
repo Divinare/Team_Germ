c4037e1661ffd676b8c09ada51c6ebc6bd873b11
broileri <jenna.lindh@luukku.com>
2014-10-05 21:26:48 +0300

    shooting projectiles

--- Germs/Assets/Scripts/Battle/Unit/RangedAttack.cs
+++ Germs/Assets/Scripts/Battle/Unit/RangedAttack.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class RangedAttack : MonoBehaviour {
 
+	public Rigidbody projectile;
+	public float bulletSpeed = 0.1f;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -14,7 +16,15 @@ public class RangedAttack : MonoBehaviour {
 	}
 
 	public void attack(GameObject target) {
+		sendProjectile (target);
 		// Not considering distance yet
 		target.GetComponent<UnitStatus> ().TakeDamage(this.GetComponent<UnitStatus> ().damage);
+	}
+
+	void sendProjectile(GameObject target) {
+		var bullet = new Rigidbody();
+		bullet = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		bullet.velocity = (target.transform.position - transform.position).normalized * bulletSpeed;
+		//bullet.velocity.z = 0; 
 	}
 }
