2625c40e98b06ce98131cf494f585aef74373a45
broileri <jenna.lindh@luukku.com>
2014-10-06 21:38:39 +0300

    moar ranged attack

---
+++ Germs/Assets/Scripts/Battle/Unit/OnCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hai, this is bullet");	
+	}
+	
+
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("omg, i'm hitting something1");
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("omg, i'm hitting something2");
+		Destroy(gameObject);
+	}
+
+
+	}
--- Germs/Assets/Scripts/Battle/Unit/RangedAttack.cs
+++ Germs/Assets/Scripts/Battle/Unit/RangedAttack.cs
@@ -2,29 +2,49 @@
 using System.Collections;
 
 public class RangedAttack : MonoBehaviour {
-
+	
 	public Rigidbody projectile;
-	public float bulletSpeed = 0.1f;
-
+	public float bulletSpeed;
+	
+	
+	public Rigidbody bullet;
+	public GameObject target;
+	
 	// Use this for initialization
 	void Start () {
+		bulletSpeed = 10;
+		bullet = null;
+		target = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		
+		
+		if (bullet != null && target != null) {
+			
+			//Debug.Log (target.GetComponent<BoxCollider>());
+			
+			
+		}
+		
 	}
-
+	
 	public void attack(GameObject target) {
-		sendProjectile (target);
+		
+		
+		this.target = target;
+		
+		// Setting up the bullet/projectile
+		bullet = new Rigidbody();
+		bullet = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		bullet.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+		bullet.velocity = (target.transform.position - transform.position).normalized * bulletSpeed;
+		
 		// Not considering distance yet
 		target.GetComponent<UnitStatus> ().TakeDamage(this.GetComponent<UnitStatus> ().damage);
 	}
-
-	void sendProjectile(GameObject target) {
-		var bullet = new Rigidbody();
-		bullet = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-		bullet.velocity = (target.transform.position - transform.position).normalized * bulletSpeed;
-		//bullet.velocity.z = 0; 
-	}
+	
+	
 }
