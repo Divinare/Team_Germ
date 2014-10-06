7f9fcbc7ff20d63062a62563b914a4e6cd75b67b
banjohirvi <risto.ollila@cs.helsinki.fi>
2014-10-06 13:24:43 +0300

    Fix to spawning friendly units

--- Germs/Assets/Scripts/Battle/Battle Initializer/BattleInitializer.cs
+++ Germs/Assets/Scripts/Battle/Battle Initializer/BattleInitializer.cs
@@ -3,10 +3,8 @@ using System.Collections;
 
 public class BattleInitializer : MonoBehaviour {
 
-	GameObject selector;
-	public string[] FriendlyGermsToSpawn;
-	public GameObject[] HostileGermsToSpawn;
-	RaycastHit hit;
+	public string[] friendlyGermsToSpawn;
+	public GameObject[] hostileGermsToSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +20,20 @@ public class BattleInitializer : MonoBehaviour {
 		GameObject matrix = GameObject.FindGameObjectWithTag ("Matrix");
 		GameObject[,] squares = matrix.GetComponent<Matrix> ().getSquares ();
 		GameObject battleStatus = GameObject.FindGameObjectWithTag ("Battle Tracker");
-		if (battleStatus != null) {
-			FriendlyGermsToSpawn = battleStatus.GetComponent<BattleStatus>().getSelectedBacsTest ().ToArray ();
+		friendlyGermsToSpawn = battleStatus.GetComponent<BattleStatus>().getSelectedBacsTest ().ToArray ();
+		if (friendlyGermsToSpawn.Length == 0) {
+			friendlyGermsToSpawn = new string[5];
+			friendlyGermsToSpawn[0] = "Gatbac";
+			friendlyGermsToSpawn[1] = "Gatbac";
+			friendlyGermsToSpawn[2] = "Gatbac";
+			friendlyGermsToSpawn[3] = "Gatbac";
+			friendlyGermsToSpawn[4] = "Gatbac";
 		}
+		Debug.Log (friendlyGermsToSpawn.Length);
 		int y = 8; // Start spawning mobs from the top to bottom
-		for (int i = 0; i < FriendlyGermsToSpawn.Length; i++) {
-			GameObject germToSpawn = GameObject.FindGameObjectWithTag("Unit Prefab Container").GetComponent<UnitPrefabContainer>().getGerm(FriendlyGermsToSpawn[i]);
+		for (int i = 0; i < friendlyGermsToSpawn.Length; i++) {
+			GameObject germToSpawn = GameObject.FindGameObjectWithTag("Unit Prefab Container").GetComponent<UnitPrefabContainer>().getGerm(friendlyGermsToSpawn[i]);
+			Debug.Log("Attempting to spawn " + germToSpawn);
 			GameObject spawnedGerm = SpawnObjectAtSquare (germToSpawn, squares [0, y]); 
 			spawnedGerm.GetComponent<UnitStatus>().setFriendlyStatus (true);
 			spawnedGerm.transform.GetChild(0).position = spawnedGerm.transform.position;
@@ -36,8 +42,8 @@ public class BattleInitializer : MonoBehaviour {
 			y -= 2;
 		}
 		y = 8;
-		for (int i = 0; i < HostileGermsToSpawn.Length; i++) {
-			GameObject spawnedGerm = SpawnObjectAtSquare (HostileGermsToSpawn[i], squares[14, y]);
+		for (int i = 0; i < hostileGermsToSpawn.Length; i++) {
+			GameObject spawnedGerm = SpawnObjectAtSquare (hostileGermsToSpawn[i], squares[14, y]);
 			spawnedGerm.GetComponent<UnitStatus>().setSquare (squares[14,y]); 
 			squares[14, y].GetComponent <SquareStatus>().setStatus ("enemy", spawnedGerm); // Set square status to indicate there is a hostile unit
 			y -= 2;
