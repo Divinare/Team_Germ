86765ecc65d8f52eec7ec3caadcd9858836f5609
broileri <jenna.lindh@luukku.com>
2014-10-09 00:32:49 +0300

    projectile is SO ready

--- Germs/Assets/Scripts/Battle/Unit/RangedHit.cs
+++ Germs/Assets/Scripts/Battle/Unit/RangedHit.cs
@@ -6,6 +6,7 @@ public class RangedHit : MonoBehaviour {
 	private int attackerGivesDamage;
 	private GameObject targetedUnit;
 	private Selector selector;
+	public ParticleSystem slimeballHit;
 
 	void Start() {
 		// If this value is assigned as damage the projectile isn't hitting the attacker (as it should) before it hits the target 
@@ -18,9 +19,11 @@ public class RangedHit : MonoBehaviour {
 
 		// Hitting the enemy
 		if (unit.GetComponent<UnitStatus>() == targetedUnit.GetComponent<UnitStatus>()) {
-			Destroy (gameObject);
+
+			ParticleSystem explosionEffect = Instantiate(slimeballHit, transform.position, transform.rotation) as ParticleSystem; // instantiating explosion effect
 			unit.GetComponent<UnitStatus>().TakeDamage(attackerGivesDamage);
 			selector.SetTargetedUnitToNull();
+			Destroy (gameObject);
 
 		}
 		// Hitting the attacker, as the bullet is spawned inside the attacker's collider
--- Germs/Assets/Scripts/Battle/Unit/UnitStatus.cs
+++ Germs/Assets/Scripts/Battle/Unit/UnitStatus.cs
@@ -75,7 +75,7 @@ public class UnitStatus : MonoBehaviour {
 	public void TakeDamage(int damage) {
 		PlaySound (0); // 0 = 'damage taken'-sound
 		currentHealth -= damage;
-		battlelog ("taken " + damage + " dmg");
+		battlelog (gameObject.name + " took " + damage + " damage!");
 	}
 	
 	public void Poisoned(int damage) {
