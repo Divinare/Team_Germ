47ded8c09368a70ba6f48542b017ed578161c313
Divinare <niemijoe@hotmail.com>
2014-10-26 21:33:17 +0200

    shop

--- Germs/Assets/Scripts/ItemStats.cs
+++ Germs/Assets/Scripts/ItemStats.cs
@@ -21,8 +21,9 @@ public class ItemStats : MonoBehaviour {
 	public Texture2D ragePotion;
 	public Texture2D speedPotion;
 
-	private float lvlUpCostFactor = 0.25f;
+	private float lvlUpCostFactor = 0.15f;
 	private float lvlUpEffectFactor = 0.25f;
+	private float lvlUpUpgradeCostFactor = 0.25f;
 
 	void Start () {
 		if (itemStats == null) {
@@ -35,10 +36,10 @@ public class ItemStats : MonoBehaviour {
 
 		initInventory ();
 
-		// in the int array the order is: 0 level,  1 cost, 2 effect (healing/damage buff amount)
-		currentItemStats.Add("healingPotion", new int[] {1, 25, 10});
-		currentItemStats.Add("ragePotion", new int[] {1, 25, 10});
-		currentItemStats.Add("speedPotion", new int[] {1, 25, 5});
+		// in the int array the order is: 0: itemUpgradeCost, 1 level,  2 cost, 3 effect (healing/damage buff amount)
+		currentItemStats.Add("healingPotion", new int[] {50, 1, 25, 10});
+		currentItemStats.Add("ragePotion", new int[] {50, 1, 25, 10});
+		currentItemStats.Add("speedPotion", new int[] {50, 1, 25, 5});
 
 		itemDescriptions.Add ("healingPotion", new string[] {"Healing Potion", "Potion", "heals a bacteria"});
 		itemDescriptions.Add ("ragePotion", new string[] {"Rage Potion", "Potion", "Increases damage, causes a bacteria to rage"});
@@ -59,6 +60,11 @@ public class ItemStats : MonoBehaviour {
 	}
 
 	public bool buyItem(string itemName) {
+		int cost = currentItemStats [itemName] [2];
+		if (gameStatus.getGold() - cost < 0) {
+			return false; // Not enough gold
+		}
+
 		bool full = true;
 		for (int i = 0; i < inventoryContent.GetLength(0); i++) {
 
@@ -69,14 +75,14 @@ public class ItemStats : MonoBehaviour {
 		if (full) {
 			return false;
 		}
-		gameStatus.decreaseGold (currentItemStats [itemName][1]);
+		gameStatus.decreaseGold (currentItemStats [itemName][2]);
 
 		KeyValuePair<string, int[]> entry = new KeyValuePair<string, int[]>(itemName, currentItemStats [itemName]);
 		for (int i = 0; i < inventoryContent.Length; i++) {
 			if(inventoryContent[i,0].Equals("empty")) {
 				inventoryContent[i,0] = itemName;
-				for(int j = 1; j <= currentItemStats[itemName].GetLength(0); j++) {
-					inventoryContent[i,j] = currentItemStats[itemName][j-1].ToString();
+				for(int j = 1; j <= currentItemStats[itemName].GetLength(0)-1; j++) {
+					inventoryContent[i,j] = currentItemStats[itemName][j].ToString();
 				}
 				break;
 			}
@@ -94,9 +100,13 @@ public class ItemStats : MonoBehaviour {
 		return 0;
 	}
 
-	public void sellItem(int selectedInventoryIndex) {
+	public bool sellItem(int selectedInventoryIndex) {
+		if (inventoryContent [selectedInventoryIndex, 0].Equals ("empty")) {
+			return false;
+		}
 		gameStatus.addGold (getValueOfItem (selectedInventoryIndex));
 		inventoryContent [selectedInventoryIndex, 0] = "empty";
+		return true;
 	}
 
 	public int getValueOfItem(int selectedInventoryIndex) {
@@ -105,31 +115,42 @@ public class ItemStats : MonoBehaviour {
 			return value;
 	}
 
-	public void levelUpItem(string key) {
-		currentItemStats [key] [0]++;
-		currentItemStats [key] [1] += (int)(currentItemStats [key] [1] * lvlUpCostFactor); // cost increase
-		currentItemStats [key] [2] += (int)(currentItemStats [key] [2] * lvlUpEffectFactor); // effect inrease
+	public bool levelUpItem(string itemName) {
+		int upgradeCost = currentItemStats [itemName] [0];
+		if (gameStatus.getGold () - upgradeCost < 0) {
+			return false; // not enough gold
+		}
+		gameStatus.decreaseGold (currentItemStats [itemName][0]);
+		currentItemStats [itemName] [0] += (int)(currentItemStats [itemName] [0] * lvlUpUpgradeCostFactor); // upgrade cost increase
+		currentItemStats [itemName] [1]++; // level increase
+		currentItemStats [itemName] [2] += (int)(currentItemStats [itemName] [2] * lvlUpCostFactor); // cost increase
+		currentItemStats [itemName] [3] += (int)(currentItemStats [itemName] [3] * lvlUpEffectFactor); // effect inrease
+		return true;
 	}
 
-	public int getItemLevel(string key, int selectedInventoryIndex) {
+	public int getItemUpgradeCost(string itemName) {
+		return currentItemStats [itemName] [0];
+	}
+
+	public int getItemLevel(string itemName, int selectedInventoryIndex) {
 		if (selectedInventoryIndex != -1) {
 			return int.Parse(inventoryContent[selectedInventoryIndex, 1]);
 		} else {
-			return currentItemStats [key] [0];
+			return currentItemStats [itemName] [1];
 		}
 	}
-	public int getItemCost(string key) {
-		return currentItemStats[key][1];
+	public int getItemCost(string itemName) {
+		return currentItemStats[itemName][2];
 	}
 	public int getItemEffect(string key, int selectedInventoryIndex) {
 		if (selectedInventoryIndex != -1) {
 			return int.Parse(inventoryContent[selectedInventoryIndex, 3]);
 		}
-		return currentItemStats[key][2];
+		return currentItemStats[key][3];
 	}
 
-	public Texture2D getItemIcon(string key) {
-		return itemIcons [key];
+	public Texture2D getItemIcon(string itemName) {
+		return itemIcons [itemName];
 	}
 
 	public string[,] getInventoryContent() {
@@ -140,15 +161,14 @@ public class ItemStats : MonoBehaviour {
 		return inventorySize;
 	}
 
-	public string getItemName(string key) {
-		return itemDescriptions [key] [0];
+	public string getItemName(string itemName) {
+		return itemDescriptions [itemName] [0];
 	}
-	public string getItemType(string key) {
-		return itemDescriptions [key] [1];
+	public string getItemType(string itemName) {
+		return itemDescriptions [itemName] [1];
 	}
-
-	public string getItemDescription(string key) {
-		return itemDescriptions[key][2];
+	public string getItemDescription(string itemName) {
+		return itemDescriptions[itemName][2];
 	}
 
 	public Dictionary<string, int[]> getCurrenItemStats() {
