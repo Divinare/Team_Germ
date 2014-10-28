08f058451b75e7dbf0ae9a0660e80b97d191b97a
broileri <jenna.lindh@luukku.com>
2014-10-28 03:27:17 +0200

    sounds again

--- Germs/Assets/Scripts/Battle/Unit/Actions/Dispel.cs
+++ Germs/Assets/Scripts/Battle/Unit/Actions/Dispel.cs
@@ -2,26 +2,26 @@
 using System.Collections;
 
 public class Dispel : MonoBehaviour {
+
 	private Selector selector;
+	public AudioClip dispelSound;
 
 	// Use this for initialization
 	void Start () {
 		selector = GameObject.FindGameObjectWithTag ("Selector").transform.GetComponent<Selector> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	public void dispelPoison(GameObject dispeller, GameObject target) {
 		target.GetComponent<UnitStatus>().removePoison();
+		PlaySound ();
 		dispeller.GetComponent<UnitStatus>().setActionCooldown(2);
 		endTurn(dispeller, target);
 	}
 
 	public void dispelStun(GameObject dispeller, GameObject target) {
 		target.GetComponent<UnitStatus>().removeStun();
+		PlaySound ();
 		dispeller.GetComponent<UnitStatus>().setActionCooldown(2);
 		endTurn(dispeller, target);
 	}
@@ -30,5 +30,9 @@ public class Dispel : MonoBehaviour {
 		dispeller.GetComponent<UnitStatus>().Deselect();
 		selector.SetTargetedUnitToNull();
 		selector.unlockInput (); // unlock input before ending turn
+	}
+
+	private void PlaySound() {
+		AudioSource.PlayClipAtPoint(dispelSound, transform.position);
 	}
 }
--- Germs/Assets/Scripts/Battle/Unit/Actions/Heal.cs
+++ Germs/Assets/Scripts/Battle/Unit/Actions/Heal.cs
@@ -4,20 +4,13 @@ using System.Collections;
 public class Heal : MonoBehaviour {
 
 	public GameObject healingEffect;
+	public AudioClip healSound;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	public void healTarget(GameObject healer, GameObject target) {
 		if (target.GetComponent<UnitStatus>().IsEnemy() == healer.GetComponent<UnitStatus>().IsEnemy()) {
 			healer.GetComponent<UnitStatus>().setActionCooldown(2);
+			AudioSource.PlayClipAtPoint(healSound, transform.position);
 			target.GetComponent<UnitStatus>().Heal ((int) (healer.GetComponent<UnitStatus>().getDmg () * 1.5));
 			float x = target.transform.position.x;
 			float y = target.transform.position.y;
