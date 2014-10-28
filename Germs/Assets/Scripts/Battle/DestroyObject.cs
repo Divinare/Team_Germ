3da5e5378d89fbeb1904aad91e4247dbc1e88e13
banjohirvi <ollilari@gmail.com>
2014-10-28 11:17:52 +0200

    Poison Init now destroys itself correctly

---
+++ Germs/Assets/Scripts/Battle/DestroyObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+	private float totalTimeBeforeDestroy;
+
+	// Use this for initialization
+	void Start () {
+	
+		totalTimeBeforeDestroy = 4f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		totalTimeBeforeDestroy -= Time.deltaTime;
+		if (totalTimeBeforeDestroy <= 0f) {
+			Destroy (this.gameObject);
+		}
+	
+	}
+}
